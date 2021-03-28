f1c5a46b9c0956d7ae7ec8aa27f830de2675b563
aitumik <aitumik@protonmail.com>
2021-03-29 02:34:58 +0300

    Created status codes for responses

--- src/Response.php
+++ src/Response.php
@@ -5,8 +5,20 @@ namespace Response;
 class Response
 {
     protected static $statusCodes = [
+        // Informational 1xx
         100 => "Continue",
-        101 => "Switching Protocols"
+        101 => "Switching Protocols",
+
+        // Success 2xx
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No content',
+        206 => 'Partial Content',
+
+        //Redirection 3xx
+        300 => 'Multiple Choices'
     ];
 }
 
